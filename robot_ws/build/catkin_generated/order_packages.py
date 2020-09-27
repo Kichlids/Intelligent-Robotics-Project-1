c72d92cd88cff16fbb6003410a47f82753f71d26
Kichlids <kichangsongks@gmail.com>
2020-09-27 18:19:00 -0500

    remove build and devel

--- robot_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/kichlids/Project1/Intelligent-Robotics-Project-1/robot_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/kichlids/Project1/Intelligent-Robotics-Project-1/robot_ws/devel;/opt/ros/kinetic".split(';') if "/home/kichlids/Project1/Intelligent-Robotics-Project-1/robot_ws/devel;/opt/ros/kinetic" != "" else []
--- robot_ws/build/robot_teleop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kichlids/Project1/Intelligent-Robotics-Project-1/robot_ws/src/robot_teleop
-# Build directory: /home/kichlids/Project1/Intelligent-Robotics-Project-1/robot_ws/build/robot_teleop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
